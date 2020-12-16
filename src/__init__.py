2b969daa12db773bfd8d505bbde51b6e62d80787
Heinrich Malan <heinrich@heinbio.me>
2020-12-16 17:01:16 +0100

    fix faking of lists

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import math
+import random
 import faker
 
 from marshmallow.fields import *
@@ -7,7 +9,7 @@ from marshmallow import missing
 # TODO: Validators
 
 fake = faker.Faker()
-
+random.seed()
 class ArangoORMAutoFixture():
     def __init__(self):
         self.field_to_faker = {
@@ -25,7 +27,8 @@ class ArangoORMAutoFixture():
             Date: self.fake_date,
             TimeDelta: self.fake_timedelta,
             Url: self.fake_url,
-            Email: self.fake_email
+            Email: self.fake_email,
+            List: self.fake_list
         }
 
         validators = {
@@ -42,50 +45,59 @@ class ArangoORMAutoFixture():
             'URL': ''
         }
 
-    def fake_string(self):
+    def fake_string(self, *args, **kwargs):
         return fake.paragraph()
 
-    def fake_uuid(self):
+    def fake_uuid(self, *args, **kwargs):
         return fake.uuid4()
 
-    def fake_number(self):
+    def fake_number(self, *args, **kwargs):
         return fake.random_number()
 
-    def fake_integer(self):
+    def fake_integer(self, *args, **kwargs):
         return fake.pyint()
 
-    def fake_decimal(self):
+    def fake_decimal(self, *args, **kwargs):
         return fake.pydecimal()
 
-    def fake_boolean(self):
+    def fake_boolean(self, *args, **kwargs):
         return fake.pybool()
 
-    def fake_float(self):
+    def fake_float(self, *args, **kwargs):
         return fake.pyfloat()
 
-    def fake_datetime(self):
+    def fake_datetime(self, *args, **kwargs):
         return fake.date_time()
 
-    def fake_naivedatetime(self):
+    def fake_naivedatetime(self, *args, **kwargs):
         return fake.date_time()
 
-    def fake_awaredatetime(self):
+    def fake_awaredatetime(self, *args, **kwargs):
         return fake.date_time(tzinfo=fake.pytimezone())
 
-    def fake_time(self):
+    def fake_time(self, *args, **kwargs):
         return fake.time_object()
 
-    def fake_date(self):
+    def fake_date(self, *args, **kwargs):
         return fake.date_object()
 
-    def fake_timedelta(self):
+    def fake_timedelta(self, *args, **kwargs):
         return fake.time_delta()
 
-    def fake_url(self):
+    def fake_url(self, *args, **kwargs):
         return fake.url()
 
-    def fake_email(self):
+    def fake_email(self, *args, **kwargs):
         return fake.email()
+
+    def fake_list(self, *args, **kwargs):
+        field = kwargs['field']
+        inner = field.inner
+        fake_func = self.field_to_faker[type(inner)]
+        MAX_LIST_SIZE = 50
+        list_size = random.randint(1, MAX_LIST_SIZE)
+        l = [fake_func()]
+        return l
 
     def generate_fake_object(self, object, key=1):
         fields = object.schema().fields
@@ -98,9 +110,10 @@ class ArangoORMAutoFixture():
             if (not field.required and field.allow_none) and fake.boolean(fake.random_int(0, 100)):
                 continue
             if not type(field.default) == type(missing) and fake.boolean(fake.random_int(0, 100)):
-                return field.default
+                data[field_key] = field.default
+                continue
             field_type = str()
-            data[field_key] = self.field_to_faker[type(field)]()
+            data[field_key] = self.field_to_faker[type(field)](field=field)
         return object(**data)
 
     def generate(self, object, n=1):
